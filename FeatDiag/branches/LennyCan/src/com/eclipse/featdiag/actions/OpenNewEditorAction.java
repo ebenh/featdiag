c01f1ea3076906f0a017d7f6efd5342221c761a4
g5eben <g5eben@5f111cea-8ae1-11de-85c4-67bf107c5648>
2009-07-13 19:24:10 +0000

    Stripped dead code

--- FeatDiag/branches/LennyCan/src/com/eclipse/featdiag/actions/OpenNewEditorAction.java
+++ FeatDiag/branches/LennyCan/src/com/eclipse/featdiag/actions/OpenNewEditorAction.java
@@ -38,67 +38,12 @@ import com.eclipse.featdiag.editors.DiagramEditor;
  *
  */
 public class OpenNewEditorAction implements IActionDelegate {
-//	boolean diagExists = false;
 	ISelection selection;
-//	DiagramEditor editor = null;
+	
 	/**
 	 * Called when option to open new feature diagram is selected
 	 * from the context menu.
 	 */
-//	public void run(IAction action) {
-//		try {
-//			IJavaElement element = (IJavaElement) ((StructuredSelection) selection)
-//					.getFirstElement();
-//			IJavaProject javaProj = element.getJavaProject();
-//			IProject proj = javaProj.getProject();
-//			IPath path = element.getPath();
-//			IFile featFile = createFile(path, javaProj.getProject());
-//			
-//			if (proj.getFullPath().isPrefixOf(path)) {
-//				int i = path.matchingFirstSegments(proj.getFullPath());
-//				path = path.removeFirstSegments(i);
-//			}
-//			IFile javaFile = proj.getFile(path);
-//			IFile classFile = FileUtils.getClassFile(javaFile);
-//
-//			if (featFile != null) {
-//				FileEditorInput input = new FileEditorInput(featFile);
-//				IWorkbenchWindow window = PlatformUI.getWorkbench()
-//						.getActiveWorkbenchWindow();
-//				editor = (DiagramEditor) window.getActivePage()
-//						.openEditor(input,
-//								"featdiag.editors.FeatureDiagramEditor");
-//				
-//				
-//		        
-//		        
-//				if (classFile != null && !getDiagExists()) {
-//					String classFilePath = classFile.getLocation().toString();
-//					editor.addMembers(classFilePath, javaFile.getFullPath().toString());
-//				}
-//			}
-//		} catch (PartInitException e) {
-//		} catch (CoreException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		// Set up a listener for file changes.
-//		IPath filePath = null;
-//		try {
-//			filePath = editor.getFilePath();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		if (filePath != null) {
-//			IFile file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(filePath);
-//			FileSaveListener listener = editor.setListener(file);
-//			file.getWorkspace().addResourceChangeListener(listener, IResourceChangeEvent.POST_BUILD);
-//		}
-//		
-//	}
-	
 	public void run(IAction action) {
 		try {
 			IJavaElement element = (IJavaElement) ((StructuredSelection) selection).getFirstElement();
@@ -131,17 +76,22 @@ public class OpenNewEditorAction implements IActionDelegate {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+//		// Set up a listener for file changes.
+//		IPath filePath = null;
+//		try {
+//			filePath = editor.getFilePath();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		if (filePath != null) {
+//			IFile file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(filePath);
+//			FileSaveListener listener = editor.setListener(file);
+//			file.getWorkspace().addResourceChangeListener(listener, IResourceChangeEvent.POST_BUILD);
+//		}
 	}
 	
-	/**
-	 * Returns whether or not the feature diagram associated
-	 * with the selected class already exists.
-	 * @return
-	 */
-//	private boolean getDiagExists() {
-//		return diagExists;
-//	}
-
 	/**
 	 * Selected class changed.
 	 */
@@ -158,32 +108,6 @@ public class OpenNewEditorAction implements IActionDelegate {
 	 * @param proj
 	 * @return
 	 */
-//	private IFile createFile(IPath path, IProject proj) {
-//		IFile retval = null;
-//		try {
-//			// Get just the name of the new .feat file
-//			path = new Path(path.lastSegment());
-//			path = path.removeFileExtension().addFileExtension("feat");
-//
-//			// Place the new file in the project directory.
-//			// Create the file if it does not exist.
-//			IPath filePath = proj.getLocation().append(path);
-//			File file = filePath.toFile();
-//			
-//			diagExists = (file == null || file.exists());
-//			file.createNewFile();
-//
-//			// Notify the workspace that the file has changed
-//			retval = proj.getFile(path);
-//			proj.refreshLocal(IResource.DEPTH_ONE, new NullProgressMonitor());
-//		} catch (IOException e) {
-//			System.out.println(e.getMessage());
-//		} catch (CoreException e) {
-//			System.out.println(e.getMessage());
-//		}
-//		return retval;
-//	}
-	
 	private IFile createFile(IPath path, IProject proj) {
 		IFile retval = null;
 		try {
@@ -195,8 +119,7 @@ public class OpenNewEditorAction implements IActionDelegate {
 			// Create the file if it does not exist.
 			IPath filePath = proj.getLocation().append(path);
 			File file = filePath.toFile();
-			
-//			diagExists = (file == null || file.exists());
+
 			file.createNewFile();
 
 			// Notify the workspace that the file has changed
